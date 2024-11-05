0887428bfcdb4ec3622d31e21c147c919c53b364
OxiazozouO <1714050472@qq.com>
2024-11-06 03:02:31 +0800

    小修改

---
+++ XnbConverter.Core/Exceptions/Texture2DReaderError.cs
@@ -0,0 +1,12 @@
+﻿namespace XnbConverter.Exceptions;
+
+public class Texture2DReaderError : XnbError
+{
+    public Texture2DReaderError(string message) : base(message)
+    {
+    }
+    
+    public Texture2DReaderError(string message, params object[] str) : base(message, str)
+    {
+    }
+}
